9b221dd135e98a2fb1077c2374ee815baf878a9d
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-04-18 19:04:03 +0530

    Time: 30 ms (67.25%) | Memory: 18.9 MB (31.01%) - LeetSync

--- 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.py
+++ 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.py
@@ -2,30 +2,30 @@ from collections import defaultdict
 
 class Solution:
     def isPossible(self, nums):
-        freq = defaultdict(int)
-        need = defaultdict(int)
+        freq=Counter(nums)
+        extend_last=Counter()
 
         for num in nums:
-            freq[num] += 1
-
-        for num in nums:
-            if freq[num] == 0:
+            if freq[num]==0:
                 continue
 
-            # Extend existing subsequence
-            if need[num] > 0:
-                need[num] -= 1
-                need[num + 1] += 1
+            elif extend_last[num-1]>0:
+                extend_last[num]+=1
+                extend_last[num-1]-=1
 
-            # Start new subsequence
-            elif freq[num + 1] > 0 and freq[num + 2] > 0:
-                freq[num + 1] -= 1
-                freq[num + 2] -= 1
-                need[num + 3] += 1
+            elif freq[num+1]>0 and freq[num+2]>0:
+                freq[num+1]-=1
+                freq[num+2]-=1
+
+                extend_last[num+2]+=1
 
             else:
                 return False
-
-            freq[num] -= 1
+            
+            freq[num]-=1
 
         return True
+
+
+
+        
