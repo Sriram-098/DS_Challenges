4f373ffdc6c330a7ae0fde92426cf2559f290c82
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-06-04 19:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (25.82%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, arr: List[int], target: int) -> int:
+        low=0
+        high=len(arr)-1
+        while low<=high:
+            mid=(low+high)//2
+            if arr[mid]==target:
+                return mid
+            elif target<=arr[mid]:
+                high=mid-1
+            else:
+                low=mid+1
+        return low
+        
