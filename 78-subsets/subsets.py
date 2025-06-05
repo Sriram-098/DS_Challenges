318032ea20b88d97d142b3a1961830194f6e08b5
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-06-05 20:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (65.17%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,18 +1,23 @@
 class Solution:
-    def ishelper(self,sub,i,res,nums):
-        if(i>=len(nums)):
-            res.append(sub.copy())
-            return
-        
-        sub.append(nums[i])
-        self.ishelper(sub,i+1,res,nums)
-        sub.remove(nums[i])
-        self.ishelper(sub,i+1,res,nums)
+    
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res=[]
+        ans=[]
         sub=[]
-        self.ishelper(sub,0,res,nums)
-        return res
+        def f(i,nums,sub,ans):
+            if i==len(nums):
+                ans.append(sub[:])
+                return
+
+            sub.append(nums[i])
+            f(i+1,nums,sub,ans)
+            sub.pop()
+            f(i+1,nums,sub,ans)
+
+
+
+
+        f(0,nums,sub,ans)
+        return ans
 
     
         
