ec97304bd416bbf2201f5fae09bd33800545348c
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-06-11 22:08:24 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (81.76%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -6,28 +6,15 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        q=deque()
-        ans=[]
-        count=0
-        q.append(root)
+        
         if not root:
             return 0
-        while q:
-            sub=[]
-            si=len(q)
-            for i in range(si):
-                root=q.popleft()
-                count+=1
-                sub.append(root.val)
+        
+        def f(root):
+            if not root:
+                return 0
+            return 1+f(root.left)+f(root.right)
+        
 
-                if root.left !=None:
-                    q.append(root.left)
-
-                if root.right!=None:
-                    q.append(root.right)
-            
-
-            ans.append(sub)
-
-        return count
+        return f(root)
         
