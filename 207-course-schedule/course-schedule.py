7c1db14dba8a69f948e907ed2d3f25e5f92a3dc9
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-06-22 15:55:09 +0530

    Time: 2 ms (90.65%) | Memory: 18.8 MB (92.96%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj=[[] for _ in range(numCourses)]
+        for u,v in prerequisites:
+            adj[u].append(v)
+        
+        indre=[0]*numCourses
+        for i in range(numCourses):
+            for j in adj[i]:
+                indre[j]+=1
+        
+        q=deque()
+        for i in range(numCourses):
+            if indre[i]==0:
+                q.append(i)
+
+        lis=[]
+        while q:
+            node=q.popleft()
+            lis.append(node)
+            for i in adj[node]:
+                indre[i]-=1
+                if indre[i]==0:
+                    q.append(i)
+        return len(lis)==numCourses
+        
