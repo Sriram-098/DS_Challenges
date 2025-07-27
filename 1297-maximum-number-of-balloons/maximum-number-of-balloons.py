689e5c197bd754aa101c6622ad725bc1fac61ef0
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-07-27 15:10:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (94.62%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,22 +1,8 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        d={"b":0 ,"a":0,"l":0,"o":0,"n":0}
-        x="balon"
-        for i in range(len(text)):
-            if text[i] in x:
-                if text[i] in d:
-                    d[text[i]]+=1
-                else:
-                    d[text[i]]=1
-        print(d)
-        min1=min(d.values())
-        min2=min(d.get('l'),d.get('o'))
-
-        if min1*2<=min2:
-            return min1
-        return min2//2
-        
-
+        d=Counter(text)
+        mini=min(d['b'],d['a'],d['l']//2,d['o']//2,d['n'])
+        return mini
         
                 
             
