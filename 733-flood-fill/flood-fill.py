ad08f158270b1825119923efa5f531f16b4f3566
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-17 18:00:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (51.07%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,20 +1,26 @@
+from collections import deque
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        q=deque()
-        original_color=image[sr][sc]
+        change=image[sr][sc]
         image[sr][sc]=color
+        q=deque()
         q.append((sr,sc))
+        dirs=((-1,0),(0,1),(1,0),(0,-1))
         vis=[[0]*len(image[0]) for _ in range(len(image))]
-        vis[sr][sc]=1
-        dirs=((0,1),(1,0),(-1,0),(0,-1))
         while q:
-            r,c=q.pop()
+            r,c=q.popleft()
             for i,j in dirs:
                 nr=i+r
                 nc=j+c
-                if 0<=nr<len(image) and 0<=nc<len(image[0]) and vis[nr][nc]==0 and image[nr][nc]==original_color:
+                if 0<=nr<len(image) and 0<=nc<len(image[0]) and image[nr][nc]==change and vis[nr][nc]==0:
                     image[nr][nc]=color
                     vis[nr][nc]=1
                     q.append((nr,nc))
-
         return image
+
+
+        
+
+    
+
+        
