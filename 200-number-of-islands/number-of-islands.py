9f86f8c11b6361b0fb077f1c202bcdf1be892d05
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-17 17:08:47 +0530

    Time: 250 ms (51.85%) | Memory: 20.3 MB (57.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        vis=[[0]*len(grid[0]) for i in range(len(grid))]
-        
+        vis=[[0]*len(grid[0]) for _ in range(len(grid))]
         count=0
-
-        def dfs(i,j,vis,grid):
-            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or vis[i][j]==1 or grid[i][j]=="0":
-                return
-
+        def dfs(i,j):
+            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or vis[i][j]==1 or grid[i][j]=='0':
+                return 
             vis[i][j]=1
-            dfs(i+1,j,vis,grid)
-            dfs(i-1,j,vis,grid)
-            dfs(i,j+1,vis,grid)
-            dfs(i,j-1,vis,grid)
-        
+            dfs(i-1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+            dfs(i+1,j)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if vis[i][j]==0 and grid[i][j]=="1":
+                if vis[i][j]==0 and grid[i][j]=='1':
                     count+=1
-                    dfs(i,j,vis,grid)
-
+                    dfs(i,j)
         return count
-
         
