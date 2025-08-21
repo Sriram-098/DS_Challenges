6d6de9682490cbc9d7c34b2a4a66302e7f5b1760
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-21 13:29:20 +0530

    Time: 798 ms (19.54%) | Memory: 19.9 MB (44.04%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,35 +1,25 @@
 from queue import PriorityQueue
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        pq=PriorityQueue()
-        effort=0
-        r=0
-        c=0
-        pq.put((effort,r,c))
-        
-        
-        vis=[[1e9]*len(heights[0]) for _ in range(len(heights))]
-        vis[0][0]=0
         dirs=((-1,0),(1,0),(0,1),(0,-1))
+        pq=PriorityQueue()
+        pq.put((0,0,0))
+        dis=[[1e9]*len(heights[0]) for _ in range(len(heights))]
+        dis[0][0]=0
         while not pq.empty():
-            
-            effort,r,c=pq.get()
-            
-            if r==len(heights)-1 and c==len(heights[0])-1:
-                return effort
-            for i,j in dirs:
-                nr=r+i
-                nc=c+j
-                
-                if 0<=nr<len(heights) and 0<=nc<len(heights[0]) and  max(effort,abs(heights[nr][nc]-heights[r][c]))<vis[nr][nc]:
-                    vis[nr][nc]=max(effort,abs(heights[nr][nc]-heights[r][c]))
-                    
-                    
-                    pq.put((max(effort,abs(heights[nr][nc]-heights[r][c])),nr,nc))
-
+            diff,i,j=pq.get()
+            print(diff,i,j)
+            if i==len(heights)-1 and j==len(heights[0])-1:
+                return diff
+            for r,c in dirs:
+                nr=i+r
+                nc=j+c
+                if 0<=nr<len(heights) and 0<=nc<len(heights[0]) and max(diff,abs(heights[i][j]-heights[nr][nc]))<dis[nr][nc]:
+                    dis[nr][nc]=max(diff,abs(heights[i][j]-heights[nr][nc]))
+                    pq.put((max(diff,abs(heights[i][j]-heights[nr][nc])),nr,nc))
         return 0
 
-            
+
 
 
         
