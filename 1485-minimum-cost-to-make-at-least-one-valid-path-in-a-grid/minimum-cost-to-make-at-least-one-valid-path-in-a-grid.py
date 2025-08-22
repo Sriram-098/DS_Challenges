df211b8a7759585232da9c42d1ddfbb154fe9785
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-22 22:57:01 +0530

    Time: 479 ms (5.12%) | Memory: 19.5 MB (41.36%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,63 +1,34 @@
 from queue import PriorityQueue
+from typing import List
+
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
-        pq=PriorityQueue()
-        pq.put((0,0,0))
-        dirs=((-1,0),(1,0),(0,1),(0,-1))
-        vis=[[0]*len(grid[0]) for _ in range(len(grid))]
-        
+        pq = PriorityQueue()
+        pq.put((0,0,0))  
+        dirs = [(-1,0),(1,0),(0,1),(0,-1)]
+        vis = [[0]*len(grid[0]) for _ in range(len(grid))]
+
         while not pq.empty():
-          
-            count,i,j=pq.get()
-            
-            
-            if vis[i][j]==1:
+            count, i, j = pq.get()
+
+            if vis[i][j]:   
                 continue
-            vis[i][j]=1
-            if i==len(grid)-1 and j==len(grid[0])-1:
+            vis[i][j] = 1   
+
+            if i == len(grid)-1 and j == len(grid[0])-1:
                 return count
-            
-          
-            
+
             for r,c in dirs:
-                nr=i+r
-                nc=j+c
-                
-                if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and vis[nr][nc]==0 and grid[i][j]==1 and nr==i and nc==j+1:
-                    pq.put((count,nr,nc))
-                    
-                    
-                elif 0<=nr<len(grid) and 0<=nc<len(grid[0]) and vis[nr][nc]==0 and grid[i][j]==2 and nr==i and nc==j-1:
-                    pq.put((count,nr,nc))
-                 
-                    
-                elif 0<=nr<len(grid) and 0<=nc<len(grid[0]) and vis[nr][nc]==0 and grid[i][j]==3 and nr==i+1 and nc==j:
-                    pq.put((count,nr,nc))
-                 
-               
-                elif 0<=nr<len(grid) and 0<=nc<len(grid[0]) and vis[nr][nc]==0 and grid[i][j]==4 and nr==i-1 and nc==j:
-                    pq.put((count,nr,nc))
-                  
-                    
-                elif 0<=nr<len(grid) and 0<=nc<len(grid[0])  and vis[nr][nc]==0:
-                     
-                    pq.put((count+1,nr,nc))
-                   
+                nr, nc = i+r, j+c
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and not vis[nr][nc]:
+                    if grid[i][j] == 1 and nr == i and nc == j+1:
+                        pq.put((count, nr, nc))
+                    elif grid[i][j] == 2 and nr == i and nc == j-1:
+                        pq.put((count, nr, nc))
+                    elif grid[i][j] == 3 and nr == i+1 and nc == j:
+                        pq.put((count, nr, nc))
+                    elif grid[i][j] == 4 and nr == i-1 and nc == j:
+                        pq.put((count, nr, nc))
+                    else:
+                        pq.put((count+1, nr, nc))
         return -1
-
-
-
-
-
-            
-
-
-
-
-
-
-
-
-      
-
-        
