494e53742a9a8a715c8928324ac403a2ebed0c0f
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-22 22:53:02 +0530

    Time: 1167 ms (5.02%) | Memory: 25.2 MB (40.82%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,26 +1,30 @@
+from collections import deque
+from queue import PriorityQueue
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        pq=PriorityQueue()
+        pq.put((0,0,0,0))
+        dirs=((0,-1),(1,0),(-1,0),(0,1))
         vis=set()
-        q=deque()
-        q.append((0,0,0,0))
         vis.add((0,0,0))
-        dirs=((0,1),(1,0),(0,-1),(-1,0))
-        while q:
-            r,c,steps,obs=q.popleft()
-            if r==len(grid)-1 and c==len(grid[0])-1:
+        while not pq.empty():
+            #print(pq.queue)
+            steps,m,i,j=pq.get()
+            
+            if i==len(grid)-1 and j==len(grid[0])-1:
                 return steps
-            for i in dirs:
-                nr=r+i[0]
-                nc=c+i[1]
+           
+            for r,c in dirs:
+                nr=i+r
+                nc=j+c
                 if 0<=nr<len(grid) and 0<=nc<len(grid[0]):
-                    newobs=obs+grid[nr][nc]
-                    if newobs<=k and (nr,nc,newobs) not in vis:
-                        q.append((nr,nc,steps+1,newobs))
-                        vis.add((nr,nc,newobs))
+                    obstacle=m+grid[nr][nc]
+                    if  m<=k and (nr,nc,obstacle) not in vis:
+                        pq.put((steps+1,obstacle,nr,nc))
+                        vis.add((nr,nc,obstacle))
+
         return -1
-
-
-        
+                    
         
 
         
