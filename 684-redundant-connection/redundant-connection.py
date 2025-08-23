473dfff0dbbaae50bc26019d2cb8ffc6337432d7
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-23 11:08:55 +0530

    Time: 4 ms (31.32%) | Memory: 18.2 MB (76.72%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,39 +1,34 @@
-class Disjointset:
+class DisjointSet:
     def __init__(self,n):
-        self.parent=[i for i in range(n)]
-        self.rank=[0]*n
-        
-    def findultpar(self,u):
+        self.parent=[i for i in range(n+1)]
+        self.rank=[0]*(n+1)
+    def findpar(self,u):
         if self.parent[u]==u:
             return self.parent[u]
-            
-        ulp=self.findultpar(self.parent[u])
+        ulp=self.findpar(self.parent[u])
         self.parent[u]=ulp
         return self.parent[u]
-        
-    def union(self,u,v):
-        par_u=self.findultpar(u)
-        par_v=self.findultpar(v)
+    def findrank(self,u,v):
+        par_u=self.parent[u]
+        par_v=self.parent[v]
         if par_u ==par_v:
             return 
-        if self.rank[par_u] <self.rank[par_v]:
+        if self.rank[par_u]>self.rank[par_v]:
+            self.parent[par_v]=par_u
+        elif self.rank[par_v]>self.rank[par_u]:
             self.parent[par_u]=par_v
-            
-        elif self.rank[par_u]>self.rank[par_v]:
-            self.parent[par_v]=par_u
         else:
-            self.parent[par_v]=par_u
-            
             self.rank[par_u]+=1
+            self.parent[par_v]=par_u
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        ds=Disjointset(len(edges)+1)
-        ans=[0,0]
+        ds=DisjointSet(len(edges))
+        ans=[]
         for u,v in edges:
-            if ds.findultpar(u)!=ds.findultpar(v):
-                ds.union(u,v)
+            if ds.findpar(u)!=ds.findpar(v):
+                ds.findrank(u,v)
             else:
-                ans=[u,v]
-        return ans
+                ans.append([u,v])
+        return ans[len(ans)-1]
         
