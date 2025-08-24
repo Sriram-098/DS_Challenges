b543c5f3f5b8c62740d26271a1a42f30259896bc
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-24 08:59:35 +0530

    Time: 387 ms (23.97%) | Memory: 19 MB (27.39%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -2,17 +2,21 @@ from functools import lru_cache
 
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        @lru_cache(None)
+        #@lru_cache(None)
+        dp=[-1]*len(arr)
         def check(i):
             if i==len(arr):
                 return 0
+            if dp[i]!=-1:
+                return dp[i]
             currmax=0
             maxi=0
             for j in range(i,min(i+k,len(arr))):
                 currmax=max(currmax,arr[j])
                 length=j-i+1
                 maxi=max(maxi,(currmax*length)+check(j+1))
-            return maxi
+                dp[i]=maxi
+            return dp[i]
 
         return check(0)
         
