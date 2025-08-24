b87c9c18feebb37593397d098b2a367db75e0d55
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-24 13:17:35 +0530

    Time: 267 ms (46.18%) | Memory: 30.6 MB (52.29%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -1,26 +1,23 @@
-from functools import lru_cache
-MOD=10**9+7
 
 class Solution:
-    def numWays(self,steps, arrLen):
-        maxPos = min(steps, arrLen-1)
-
-        @lru_cache(None)
-        def dfs(i, pos):
-            if i == 0:
-                return 1 if pos == 0 else 0
-            ans = dfs(i-1, pos)  # stay
-            if pos > 0:
-                ans += dfs(i-1, pos-1)  # left
-            if pos < maxPos:
-                ans += dfs(i-1, pos+1)  # right
-            return ans % MOD
-
-        return dfs(steps, 0)
-
-
-            
-
+    def numWays(self, steps: int, arrLen: int) -> int:
+        mod=10**9+7
+        pos=min(steps,arrLen-1)
+        dp=[[-1]*(steps+1) for _ in range(pos+1)]
         
+       
+        def dfs(i,steps):
+            if steps==0:
+                return 1 if i==0 else 0
+            if dp[i][steps]!=-1:
+                return dp[i][steps]
+            result=dfs(i,steps-1)
+            if i>0:
+                result=(result+dfs(i-1,steps-1))%mod
+            if i<pos:
+                result=(result+dfs(i+1,steps-1))%mod
+            dp[i][steps]=result
+            return dp[i][steps]
 
-
+        return dfs(0,steps)
+        
