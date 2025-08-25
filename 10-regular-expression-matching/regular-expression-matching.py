a82243032cef298f9baf15365168d85a52c68bb3
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-25 21:12:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (46.95%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,14 +1,29 @@
-class Solution(object):
-    def isMatch(self, text: str, pattern: str) -> bool:
-        dp = [[False] * (len(pattern) + 1) for _ in range(len(text) + 1)]
+from functools import lru_cache
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        @lru_cache(None)
+        def f(i,j):
+            if i<0 and j<0:
+                return True
+            if i>=0 and j<0:
+                return False
+            if i<0 and j>=0:
+                while j>=0:
+                    if p[j]=='*':
+                        j-=2
+                    else:
+                        return False
+                    
+                return True
 
-        dp[-1][-1] = True
-        for i in range(len(text), -1, -1):
-            for j in range(len(pattern) - 1, -1, -1):
-                first_match = i < len(text) and pattern[j] in {text[i], "."}
-                if j + 1 < len(pattern) and pattern[j + 1] == "*":
-                    dp[i][j] = dp[i][j + 2] or first_match and dp[i + 1][j]
+
+            if p[j]=='.' or s[i]==p[j]:
+                return f(i-1,j-1)
+            if p[j]=='*':
+                if j>0 and (p[j-1]==s[i] or p[j-1]=='.'):
+                    return f(i,j-1) or f(i,j-2) or f(i-1,j)
                 else:
-                    dp[i][j] = first_match and dp[i + 1][j + 1]
-
-        return dp[0][0]
+                    return f(i,j-2)
+                
+            return False
+        return f(len(s)-1,len(p)-1)
