6baf07c4bb2bf4e40ffbb08e4d4d0e0407b1e8d7
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-08-25 14:17:52 +0530

    Time: 31 ms (93.59%) | Memory: 20.1 MB (68.35%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,27 +1,21 @@
+from functools import lru_cache
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp=[[-1]*len(word2) for _ in range(len(word1))]
-
+        @lru_cache(None)
         def f(i,j):
-
             if i<0:
-                return j+1
+                return j
             if j<0:
-                return i+1
-            if dp[i][j]!=-1:
-                return dp[i][j]
+                return i
             if word1[i]==word2[j]:
-                dp[i][j]=f(i-1,j-1)
-                return dp[i][j]
-            
-            insertion=1+f(i,j-1)
-            deletion=1+f(i-1,j)
+                return f(i-1,j-1)
+            insert,delete,replace=1e9,1e9,1e9
+            insert =1+f(i,j-1)
+            delete=1+f(i-1,j)
             replace=1+f(i-1,j-1)
-            dp[i][j]=min(insertion,deletion,replace)
-            return dp[i][j]
+            return min(insert,delete,replace)
 
 
 
-    
-        return f(len(word1)-1,len(word2)-1)
+        return f(len(word1)-1,len(word2)-1)+1
         
