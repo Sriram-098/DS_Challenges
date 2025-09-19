ef4e39b0a1e860b3744dcb12f28214fdbd48bee1
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-09-19 10:41:58 +0530

    Time: 3 ms (96.88%) | Memory: 18.5 MB (75.55%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,18 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        maxgo=0
-        count=0
-        end=0
+        if len(nums)==1:
+            return 0
+        maxreach=0
         jumps=0
-        for i in  range(len(nums)-1):
-            maxgo=max(maxgo,i+nums[i])
+        end=0
+        for i in range(len(nums)):
+            maxreach=max(maxreach,i+nums[i])
             if i==end:
                 jumps+=1
-                end=maxgo
+                end=maxreach
                 if end>=len(nums)-1:
                     break
-            
         return jumps
+            
 
-        
+            
         
