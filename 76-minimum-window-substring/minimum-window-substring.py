d8b6ae3352c0d572cdebb56a0e87e7c6ecb391de
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-09-21 21:38:16 +0530

    Time: 207 ms (19.05%) | Memory: 18.6 MB (6.21%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,40 +1,43 @@
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        d=Counter(t)
+    def minWindow(self, s: str, p: str) -> str:
+        d=Counter(p)
+        #print(d)
+        ind=-1
+        mini=1e9
         l=0
         r=0
-        maxlen=1e9
-        x=len(t)
-        si=-1
         count=0
-        while(r<len(s)):
-            if s[r] in d and d[s[r]]>0:
+        while r<len(s):
+            if d[s[r]]>0:
                 d[s[r]]-=1
                 count+=1
-            else :
-                d[s[r]]=d.get(s[r],0)-1
-            
-            while(count==len(t)):
-                if (r-l+1)<maxlen:
-                    maxlen=r-l+1
-                    si=l
+            else:
+                if s[r] in d:
+                    d[s[r]]-=1
+                else:
+                    d[s[r]]=-1
+                
+            while count==len(p) :
+                print(l,r,count)
+                leng=r-l+1
+                if leng<mini:
+                    ind=l
+                    mini=r-l+1
                 
                 d[s[l]]+=1
-                if d[s[l]]>0:
+                if d[s[l]] >0:
                     count-=1
-                l+=1
-            r+=1
-        #print(s[si:si+maxlen])
-        return "" if si==-1 else s[si:(si+maxlen)]
-
-            
-
-            
-
-
-
-
                 
-
-
+        
+                l+=1
+            
+            r+=1
+                
+        return s[ind:(ind+mini)] if ind!=-1 else ""
+                    
+                    
+            
+            
+    
+        
         
