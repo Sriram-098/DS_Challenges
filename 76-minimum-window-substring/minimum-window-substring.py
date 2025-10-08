29462b175197e9446d798c57fbdd8c92667cf1cf
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-08 22:39:42 +0530

    Time: 123 ms (35.90%) | Memory: 18.5 MB (6.52%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,43 +1,56 @@
+from collections import Counter
 class Solution:
-    def minWindow(self, s: str, p: str) -> str:
-        d=Counter(p)
-        #print(d)
-        ind=-1
-        mini=1e9
+    def minWindow(self, s: str, t: str) -> str:
+        d=Counter(t)
+        le=len(d)
         l=0
         r=0
         count=0
+        maxlength=1e9
+        st=-1
         while r<len(s):
-            if d[s[r]]>0:
-                d[s[r]]-=1
+            
+            if s[r] in d and d[s[r]]>0:
                 count+=1
+                d[s[r]]-=1
             else:
-                if s[r] in d:
-                    d[s[r]]-=1
-                else:
-                    d[s[r]]=-1
-                
-            while count==len(p) :
-                print(l,r,count)
-                leng=r-l+1
-                if leng<mini:
-                    ind=l
-                    mini=r-l+1
+                d[s[r]]=d.get(s[r],0)-1
+            #print(d)
+
+            while count==len(t):
+                if r-l+1<maxlength:
+                    print(l)
+                    st=l
+                    maxlength=r-l+1
                 
                 d[s[l]]+=1
-                if d[s[l]] >0:
+                if d[s[l]]>0:
                     count-=1
-                
-        
+
                 l+=1
-            
             r+=1
+        print(l)
+        return s[st:st+maxlength] if st!=-1 else ""
+
+
+
+      
+
+            
+
+            
+
+
+
+
                 
-        return s[ind:(ind+mini)] if ind!=-1 else ""
-                    
-                    
+
+
+          
+                
+
+                
             
             
-    
-        
+
         
