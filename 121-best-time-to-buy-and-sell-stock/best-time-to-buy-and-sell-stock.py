ce2142400b8b9e305b54f696590694d33d43ec2d
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-09 14:48:01 +0530

    Time: 131 ms (25.18%) | Memory: 26.9 MB (78.02%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        maxi=0
         mini=1e9
-        profit=0
         for i in range(len(prices)):
             mini=min(prices[i],mini)
-            profit=max(profit,prices[i]-mini)
-        return profit
-
+            maxi=max(maxi,prices[i]-mini)
+        return maxi
         
