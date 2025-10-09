3ecccf518579f236a0decccccc09906a75fe8a6b
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-09 16:06:50 +0530

    Time: 79 ms (6.64%) | Memory: 28.5 MB (41.77%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -3,15 +3,13 @@ class Solution:
         l=0
         r=0
         count=0
-        while(r<len(nums)):
+        while r<len(nums):
             if nums[r]==0:
                 l=r
             
-            while(r<len(nums) and nums[r]==0):
+            while r<len(nums) and nums[r]==0:
                 count+=(r-l+1)
                 r+=1
             r+=1
-
         return count
-
         
