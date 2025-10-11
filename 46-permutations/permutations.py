bc23f98be33275afefa4aacad37d2719ae2df626
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-11 22:48:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (34.19%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,18 +1,10 @@
+from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result=[]
-        def f(i):
-            if i==len(nums):
-                result.append(nums[:])
-                return 
-
-            for j in range(i,len(nums),1):
-                nums[i],nums[j]=nums[j],nums[i]
-                f(i+1)
-                nums[i],nums[j]=nums[j],nums[i]
-
-
-        f(0)
+        for sub in permutations(nums):
+            result.append(sub)
         return result
+        
 
         
