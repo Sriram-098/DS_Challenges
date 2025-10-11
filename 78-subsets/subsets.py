56b01024b71eb2828b7b41c85a153e41d4d2f658
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-11 22:53:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.79%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,8 +2,17 @@ from itertools import combinations
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result=[]
-        for i in range(len(nums)+1):
-            for sub in combinations(nums,i):
-                result.append(list(sub))
+        def f(i,sub):
+            if i==len(nums):
+                result.append(sub[:])
+                return 
+            
+            sub.append(nums[i])
+            f(i+1,sub)
+            sub.pop()
+            f(i+1,sub)
+        
+
+        f(0,[])
         return result
         
