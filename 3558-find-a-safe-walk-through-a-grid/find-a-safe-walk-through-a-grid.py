9c4450c084d44073f51be6712f78020914006119
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-11 22:35:00 +0530

    Time: 291 ms (43.56%) | Memory: 18.4 MB (55.40%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
@@ -1,19 +1,20 @@
+from queue import PriorityQueue
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
         mat=[[1e9]*len(grid[0]) for _ in range(len(grid))]
         mat[0][0]=grid[0][0]
-        q=deque()
-        q.append((0,0))
+        q=PriorityQueue()
+        q.put((0,0,0))
         dirs=((-1,0),(0,1),(1,0),(0,-1))
-        while q:
-            r,c=q.popleft()
+        while not q.empty():
+            remain,r,c=q.get()
             for i in range(len(dirs)):
                 nr=r+dirs[i][0]
                 nc=c+dirs[i][1]
                 if 0<=nr<len(grid) and 0<=nc<len(grid[0]):
                     if mat[r][c]+grid[nr][nc]<mat[nr][nc]:
                         mat[nr][nc]=mat[r][c]+grid[nr][nc]
-                        q.append((nr,nc))
+                        q.put((mat[nr][nc],nr,nc))
         print(mat)
         return True if health-mat[len(grid)-1][len(grid[0])-1]>=1 else False
 
