2b092202b8d391edaeff5eedad2f0f16c7c50bb8
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-11 22:46:16 +0530

    Time: 3 ms (31.93%) | Memory: 18 MB (34.19%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,29 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ans=[] 
-
-        def swap(i,j,nums):
-            temp=nums[i]
-            nums[i]=nums[j]
-            nums[j]=temp
-        def f(i,nums):
+        result=[]
+        def f(i):
             if i==len(nums):
-                ans.append(nums[:])
+                result.append(nums[:])
                 return 
-            for j in range(i,len(nums)):
-                swap(i,j,nums)
-                f(i+1,nums)
-                swap(i,j,nums)
 
-        f(0,nums)
-        return ans
-        
+            for j in range(i,len(nums),1):
+                nums[i],nums[j]=nums[j],nums[i]
+                f(i+1)
+                nums[i],nums[j]=nums[j],nums[i]
 
 
-
-
-
-
-
+        f(0)
+        return result
 
         
