f551030a33242341091d2773d2b844316bd010d9
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-16 00:02:05 +0530

    Time: 23 ms (34.04%) | Memory: 19.4 MB (39.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,49 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-
-        lis1=self.uptoleft(height)
-        lis2=self.uptoright(height)
-        
-
-        ans=0
+        r=[0]*len(height)
+        l=[0]*len(height)
+        maxi=0
         for i in range(len(height)):
-            if height[i]<lis1[i] and height[i]<lis2[i]:
-                m=min(lis1[i],lis2[i])
-                ans+=(m-height[i])
-        return ans
-
-
-
-    def uptoleft(self,height):
-        st=[]
-        lis=[0]*len(height)
-        m=0
-
-        for i in range(len(height)):
-            m=max(height[i],m)
-            lis[i]=m 
-
-        return lis
-
-    def uptoright(self,height):
-        st=[]
-        lis=[0]*len(height)
-        m=0
-
+            maxi=max(height[i],maxi)
+            l[i]=maxi
+        maxi=0
         for i in range(len(height)-1,-1,-1):
-            m=max(height[i],m)
-            lis[i]=m
-            
-           
-        return lis
+            maxi=max(height[i],maxi)
+            r[i]=maxi
 
-
-
-
-
-    
-
-
-
-    
+        water=0
+        mini=1e9
+        for i in range(len(height)):
+            if height[i]<l[i] and height[i]<r[i]:
+                mini=min(l[i],r[i])
+                water+=(mini-height[i])
+        return water
         
