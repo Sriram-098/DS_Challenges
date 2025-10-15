1532fd1b37ca0fbef6d54d502b997a307c2dbf21
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-15 10:41:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (20.31%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,38 +1,42 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         ans=[-1,-1]
-        a=self.lower(nums,target)
-        b=self.upper(nums,target)
-        print(a,b)
-        if a<len(nums) and b<len(nums) and (nums[a]!=target and nums[b]!=target) or len(nums)==0:
+        
+        
+        def lower():
+            l=0
+            r=len(nums)-1
+            while l<r:
+                mid=(l+r)//2
+                if nums[mid]<target:  
+                    l=mid+1
+                else:
+                    r=mid
+            return l
+
+        def upper():
+            l=0
+            r=len(nums)-1
+            while l<=r:
+                mid=(l+r)//2
+                if nums[mid]<=target:
+                    l=mid+1
+                else:
+                    r=mid-1
+            return l-1
+        
+        a=lower()
+        b=upper()
+        if a<0 or b<0 or a>=len(nums) or nums[a]!=target:
             return ans
-        ans=[a,b]
+        ans[0]=a
+        ans[1]=b
+        
         return ans
 
-    def lower(self,nums,target):
-        low=0
-        high=len(nums)-1
-        while(low<high):
-            mid=int((low+high)/2)
-            if nums[mid]<target:
-                low=mid+1
-            else:
-                high=mid
+        
+
 
             
-
-        return low
-
-    def upper(self,nums,target):
-        low=0
-        high=len(nums)-1
-        while(low<=high):
-            mid=(low+high)//2
-            
-            if nums[mid]<=target:
-                low=mid+1
-            else:
-                high=mid-1
-
-        return low-1
+        
         
