8a81b5f74611ffa7a67f6aeca551c8ab9724a389
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-16 00:37:59 +0530

    Time: 646 ms (7.71%) | Memory: 31.6 MB (30.89%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,33 +1,31 @@
 class Solution:
-    def minDays(self, days: List[int], m: int, k: int) -> int:
-        if m*k>len(days):
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        if len(bloomDay)<m*k:
             return -1
-        ans=max(days)
+        def f(x,k):
+            count=0
+            ans=0
+            for i in range(len(bloomDay)):
+                if bloomDay[i]<=x:
+                    count+=1
+                else:
+                    ans+=(count//k)
+                    count=0
+            ans+=(count//k)
+            return ans
 
-        low=min(days)
-        high=max(days)
-        while(low<=high):
-            mid=(low+high)//2
-            check=self.f(days,mid,k)   
+        l=min(bloomDay)
+        r=sum(bloomDay)
+        ans=-1
+        while l<=r:
+            mid=(l+r)//2
+            check=f(mid,k)
+            print(mid,check)
             if check>=m:
                 ans=mid
-                high=mid-1
+                r=mid-1
             else:
-                low=mid+1
+                l=mid+1
+        return ans
 
-        return ans     
-
-
-    def f(self,days,x,k):
-        count=0
-        req=0
-        for i in range(len(days)):
-            if days[i]<=x:
-                count+=1
-            else:
-                req+=int(count/k)
-                count=0
-        if days[len(days)-1]<=x:
-            req+=int(count/k)
         
-        return req
