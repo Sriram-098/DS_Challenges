78b0440fbfcea762a728444919468eec3a2af97d
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-10-17 10:11:40 +0530

    Time: 11 ms (57.24%) | Memory: 18 MB (39.26%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,26 +1,31 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        result=[]
         nums.sort()
-        def generate(i,sub,target):
-            if i==len(nums):
-                if target==0:
-                    result.append(sub[:])
-                return 
+        result=[]
+        def backtrack(i,sub,target):
+            
             if target==0:
                 result.append(sub[:])
-                return 
-            if nums[i]>target:
                 return
+            if i==len(nums):
+                return 
 
-            if nums[i]<=target:
+            
+            if target>=nums[i]:
                 sub.append(nums[i])
-                generate(i,sub,target-nums[i])
+                backtrack(i,sub,target-nums[i])
                 sub.pop()
 
-            generate(i+1,sub,target)
+            backtrack(i+1,sub,target)
+           
+             
 
-        generate(0,[],target)
+    
+                
+
+
+        sub=[]
+        backtrack(0,sub,target)
         return result
 
 
