4d9fc6b26d8e309ce6b430af9d018f2b2d2d0028
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-12-20 15:32:20 +0530

    Time: 4 ms (80.57%) | Memory: 18.8 MB (81.76%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -2,29 +2,29 @@ from queue import PriorityQueue
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         adj=[[] for _ in range(n)]
+        for u,v,wt in flights:
+            adj[u].append([v,wt])
         
-        for u,v,charge in flights:
-            adj[u].append((v,charge))
-        print(adj)
         pq=PriorityQueue()
         pq.put((0,0,src))
-        ans=[1e9]*n
-        ans[src]=0
-        while not pq.empty():
-            
-            limit,charge,currstop=pq.get()
-            if limit>k:
+        dis=[1e9]*n
+        dis[src]=0
+        while pq.qsize()>0:
+            stops,effort,node=pq.get()
+            if stops >k:
                 continue
-            for i in adj[currstop]:
-                v=i[0]
-                weight=i[1]
-                if limit<=k and charge+weight<ans[v]:
-                    ans[v]=charge+weight
-                    pq.put((limit+1,ans[v],v))
+            for nei,wt in adj[node]:
+                if wt+effort<dis[nei]:
+                    dis[nei]=wt+effort
 
-        return ans[dst] if ans[dst]!=1e9 else -1
+                    pq.put((stops+1,dis[nei],nei))
 
-                    
+        return dis[dst] if dis[dst]!=1e9 else -1
 
-            
+       
+        
+
+
+        
+
         
