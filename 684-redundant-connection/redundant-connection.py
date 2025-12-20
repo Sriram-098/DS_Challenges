798429fbada8f76f2628435816365e832ce2f3b6
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2025-12-20 16:54:09 +0530

    Time: 4 ms (31.03%) | Memory: 17.9 MB (99.13%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -2,24 +2,31 @@ class DisjointSet:
     def __init__(self,n):
         self.parent=[i for i in range(n+1)]
         self.rank=[0]*(n+1)
+
     def findpar(self,u):
         if self.parent[u]==u:
             return self.parent[u]
+
         ulp=self.findpar(self.parent[u])
         self.parent[u]=ulp
         return self.parent[u]
-    def findrank(self,u,v):
+
+    def assrank(self,u,v):
         par_u=self.parent[u]
         par_v=self.parent[v]
-        if par_u ==par_v:
+
+        if par_u==par_v:
             return 
+
         if self.rank[par_u]>self.rank[par_v]:
             self.parent[par_v]=par_u
-        elif self.rank[par_v]>self.rank[par_u]:
-            self.parent[par_u]=par_v
-        else:
-            self.rank[par_u]+=1
+
+        elif self.rank[par_u]<self.rank[par_v]:
             self.parent[par_v]=par_u
+        else:
+            self.parent[par_v]=par_u
+            self.rank[par_u]+=1
+
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
@@ -27,8 +34,12 @@ class Solution:
         ans=[]
         for u,v in edges:
             if ds.findpar(u)!=ds.findpar(v):
-                ds.findrank(u,v)
+                ds.assrank(u,v)
+
             else:
                 ans.append([u,v])
         return ans[len(ans)-1]
+
+
+    
         
