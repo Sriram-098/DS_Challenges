2f676205288b766f93697158ec5b9be344739bb8
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2026-05-28 23:56:26 +0530

    Time: 4 ms (35.62%) | Memory: 19.4 MB (40.96%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,31 +1,37 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh=0
+
+        vis=[[0]*len(grid[0]) for _ in range(len(grid))]
+        fresh =0
         q=deque()
+        time=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]==1:
                     fresh+=1
                 elif grid[i][j]==2:
                     q.append((i,j))
-        vis=[[0]*len(grid[0]) for _ in range(len(grid))]
-        dirs=((-1,0),(0,1),(1,0),(0,-1))
-        time=0
+        dirs=((0,-1),(1,0),(0,1),(-1,0))
         while q:
-            qsize=len(q)
-            for i in range(qsize):
+            qsi=len(q)
+            for i in range(qsi):
                 r,c=q.popleft()
-                for i in range(len(dirs)):
-                    nr=r+dirs[i][0]
-                    nc=c+dirs[i][1]
-                    if nr>=0 and nc>=0 and nr<len(grid) and nc<len(grid[0]) and grid[nr][nc]==1 and  vis[nr][nc]==0:
+                for j in range(len(dirs)):
+                    nr=r+dirs[j][0]
+                    nc=c+dirs[j][1]
+                
+                    if nr>=0 and nc>=0 and nr<len(grid) and nc<len(grid[0]) and grid[nr][nc]==1 and vis[nr][nc]==0:
                         fresh-=1
-                        grid[nr][nc]=2
-                        q.append((nr,nc))
                         vis[nr][nc]=1
+                        q.append((nr,nc))
+                        grid[nr][nc]=2
+
             if q:
                 time+=1
-
-        if fresh==0:
+        if fresh ==0:
             return time
-        return -1
+        else:
+            return -1
+            
+            
+        
