035758a6841dd63f7aca38f51e02a06355ccdbaa
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2026-05-28 08:23:14 +0530

    Time: 91 ms (41.03%) | Memory: 19.7 MB (73.80%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,56 +1,38 @@
-from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        d=Counter(t)
-        le=len(d)
         l=0
         r=0
-        count=0
-        maxlength=1e9
+        d=Counter(t)
+        maxi=1e9
         st=-1
+        count=0
         while r<len(s):
-            
             if s[r] in d and d[s[r]]>0:
                 count+=1
                 d[s[r]]-=1
+
             else:
                 d[s[r]]=d.get(s[r],0)-1
-            #print(d)
-
+            
             while count==len(t):
-                if r-l+1<maxlength:
-                    print(l)
+                if r-l+1<maxi:
+                    maxi=r-l+1
                     st=l
-                    maxlength=r-l+1
-                
+
                 d[s[l]]+=1
                 if d[s[l]]>0:
                     count-=1
-
+                
                 l+=1
             r+=1
-        print(l)
-        return s[st:st+maxlength] if st!=-1 else ""
 
+        return s[st:st+maxi] if maxi!=1e9 else ""
 
-
-      
-
-            
 
             
 
 
-
-
-                
-
-
-          
-                
-
-                
             
-            
+
 
         
