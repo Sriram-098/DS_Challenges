cf3693751fb0dbf4a101004009e04245c79855ae
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2026-05-28 15:57:39 +0530

    Time: 7 ms (8.97%) | Memory: 19.4 MB (15.08%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -1,25 +1,35 @@
-from collections import Counter
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         freq=Counter(s)
-        se=set()
         st=[]
+        se=set()
         for i in range(len(s)):
             freq[s[i]]-=1
             if s[i] in se:
                 continue
+
             while st and st[-1]>s[i] and freq[st[-1]]>=1:
                 popped=st.pop()
                 se.remove(popped)
+            
+            se.add(s[i])
+            st.append(s[i])
+            print(s,st)
+        return "".join(st)
+        
+        # freq=Counter(s)
+        # se=set()
+        # st=[]
+        # for i in range(len(s)):
+        #     freq[s[i]]-=1
+        #     if s[i] in se:
+        #         continue
+        #     while st and st[-1]>s[i] and freq[st[-1]]>=1:
+        #         popped=st.pop()
+        #         se.remove(popped)
                 
 
-            st.append(s[i])
-            se.add(s[i])
-        print(st)
-        return "".join(st)
-            
-
-
-        
-
-        
+        #     st.append(s[i])
+        #     se.add(s[i])
+        # print(st)
+        # return "".join(st)
