0095a65f5de1c8e5b39916f8d6e1e24909cbcf35
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2026-05-29 00:56:50 +0530

    Time: 3 ms (61.60%) | Memory: 19.6 MB (80.07%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,45 +1,41 @@
-class DisjointSet:
+class Disjointset():
     def __init__(self,n):
-        self.parent=[i for i in range(n+1)]
+        self.par=[i for i in range(n+1)]
         self.rank=[0]*(n+1)
 
     def findpar(self,u):
-        if self.parent[u]==u:
-            return self.parent[u]
+        if self.par[u]==u:
+            return self.par[u]
 
-        ulp=self.findpar(self.parent[u])
-        self.parent[u]=ulp
-        return self.parent[u]
+        ulp=self.findpar(self.par[u])
+        self.par[u]=ulp
+        return self.par[u]
 
-    def assrank(self,u,v):
-        par_u=self.parent[u]
-        par_v=self.parent[v]
+    def findrank(self,u,v):
+        par_u=self.findpar(u)
+        par_v=self.findpar(v)
 
         if par_u==par_v:
             return 
 
         if self.rank[par_u]>self.rank[par_v]:
-            self.parent[par_v]=par_u
+            self.par[par_v]=par_u
 
-        elif self.rank[par_u]<self.rank[par_v]:
-            self.parent[par_v]=par_u
+        elif self.rank[par_v]>self.rank[par_u]:
+            self.par[par_u]=par_v
+
         else:
-            self.parent[par_v]=par_u
             self.rank[par_u]+=1
-
+            self.par[par_v]=par_u
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        ds=DisjointSet(len(edges))
+        ds=Disjointset(len(edges))
         ans=[]
         for u,v in edges:
             if ds.findpar(u)!=ds.findpar(v):
-                ds.assrank(u,v)
-
+                ds.findrank(u,v)
             else:
                 ans.append([u,v])
         return ans[len(ans)-1]
-
-
-    
         
