61a6d0ceac173a649471f93b0a2bb54b80900b4e
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2026-05-29 09:22:11 +0530

    Time: 57 ms (68.58%) | Memory: 23.9 MB (78.17%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -1,33 +1,15 @@
 class StockSpanner:
 
     def __init__(self):
-        self.st=[]
-        self.i=0 
-        
+        # The stack will store tuples of: (price, span)
+        self.st = [] 
 
     def next(self, price: int) -> int:
-       
-        while self.st and price >=self.st[-1][0]:
-            self.st.pop()
-        span=0
-        if not self.st:
-            span=self.i+1
-        else:
-            span=self.i-self.st[-1][1]
+        # Every day starts with a base span of 1 (the day itself)
+        span=1
+        while self.st and price>=self.st[-1][0]:
+            span+=self.st.pop()[1]
 
-        self.st.append((price,self.i))
-        self.i+=1
+        self.st.append((price,span))
+        
         return span
-        
-        
-        
-
-
-        
-
-        
-
-
-# Your StockSpanner object will be instantiated and called as such:
-# obj = StockSpanner()
-# param_1 = obj.next(price)
