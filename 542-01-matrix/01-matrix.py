ef7c1b2da5357fe97eecad8596de037f3317f344
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2026-06-12 09:18:49 +0530

    Time: 134 ms (42.19%) | Memory: 22.3 MB (46.33%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,26 +1,21 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         q=deque()
-        vis=[[0]*len(mat[0]) for _ in range(len(mat))]
+        vis=[[0]*(len(mat[0])) for _ in range(len(mat))]
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if mat[i][j]==0:
                     q.append((i,j))
                     vis[i][j]=1
-                    
-        
-        dirs=((0,-1),(-1,0),(0,1),(1,0))
-        while q:
-            r,c=q.popleft()
-            for i in range(len(dirs)):
-                nr=r+dirs[i][0]
-                nc=c+dirs[i][1]
-                if nr>=0 and nc>=0 and nr<len(mat) and nc <len(mat[0]) and vis[nr][nc]==0 and mat[nr][nc]>0:
-                    
-                    mat[nr][nc]=1+mat[r][c]
+        dirs=((0,1),(1,0),(-1,0),(0,-1))
+        while len(q)>0:
+            i,j=q.popleft()
+            for r,c in dirs:
+                nr=i+r
+                nc=j+c
+                if 0<=nr<len(mat) and 0<=nc<len(mat[0]) and mat[nr][nc]>0 and vis[nr][nc]==0:
+                    mat[nr][nc]=1+mat[i][j]
                     vis[nr][nc]=1
                     q.append((nr,nc))
-
         return mat
-
         
