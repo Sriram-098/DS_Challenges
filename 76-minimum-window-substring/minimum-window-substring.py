2ef55121ec277c0ff8adef51496f07e8ad58c75b
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2026-06-12 11:37:10 +0530

    Time: 94 ms (40.73%) | Memory: 19.6 MB (73.99%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,37 +2,28 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         l=0
         r=0
-        d=Counter(t)
         maxi=1e9
-        st=-1
+        d=Counter(t)
         count=0
+        st=-1
         while r<len(s):
             if s[r] in d and d[s[r]]>0:
-                count+=1
+                count+=1 
                 d[s[r]]-=1
-
             else:
                 d[s[r]]=d.get(s[r],0)-1
-            
+
             while count==len(t):
                 if r-l+1<maxi:
                     maxi=r-l+1
                     st=l
-
                 d[s[l]]+=1
                 if d[s[l]]>0:
                     count-=1
-                
                 l+=1
             r+=1
+        return s[st:st+maxi] if maxi !=1e9 else ""
 
-        return s[st:st+maxi] if maxi!=1e9 else ""
-
-
-            
-
-
-            
-
+                
 
         
