2f64a225c4f6ffadeac0d142499196295260d942
sriram senapathi <160108496+Sriram-098@users.noreply.github.com>
2026-09-16 18:05:08 +0530

    Time: 159 ms (18.18%) | Memory: 19.7 MB (52.74%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,24 +1,29 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        h=[0]*26
+        d={}
         l=0
         r=0
-        maxlen=0
-        maxfre=0
-        while(r<len(s)):
-            h[ord(s[r])-ord('A')]+=1
-            maxfre=max(h[ord(s[r])-ord('A')],maxfre)
-
-            while((r-l+1)-maxfre >k):
-                h[ord(s[l]) - ord('A')]-=1
-                for i in range(26):
-                    maxfre=max(maxfre,h[i])
-
-                l+=1
+        maxi=0
+        maxfreq=0
+        while r<len(s):
+            d[s[r]]=d.get(s[r],0)+1
+            # print(d)
+            maxfreq=max(d.values())
             
-            if((r-l+1)-maxfre <=k):
-                maxlen=max(maxlen,(r-l+1))
+            while ((r-l+1)-maxfreq)>k:
+                d[s[l]]=d.get(s[l])-1
+                if d[s[l]]==0:
+                    del d[s[l]]
 
+                maxfreq=max(d.values())
+                l+=1
+
+
+            maxi=max(r-l+1,maxi)
             r+=1
-        return maxlen
+        return maxi
+
+
+
+         
         
